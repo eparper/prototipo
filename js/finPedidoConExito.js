74d8511cd14db611ac9b83f05ae853841c26aa9d
eparper <eparper111@g.educaand.es>
2026-05-11 12:59:33 +0200

    Add files via upload

---
+++ js/finPedidoConExito.js
@@ -0,0 +1,19 @@
+//BOTÓN VER TODOS LOS PEDIDOS QUE HAS REALIZADO
+
+let bVerTodosPedidos = document.getElementById("bVerTodosPedidos");
+let bAtras2 = document.getElementById("bAtras2");
+let bAtras4 = document.getElementById("bAtras4");
+
+bVerTodosPedidos.onclick = function () {
+    window.location.href = 'confirmarPedido.html';
+}
+
+
+
+bAtras2.onclick = function () {
+    window.location.href = 'cartaMenus.html';
+}
+
+bAtras4.onclick = function () {
+    window.location.href = 'cartaMenus.html';
+}
